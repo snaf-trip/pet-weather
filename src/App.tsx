a02999e159be8724cdf8c75b96193bef8f3d2cad
snaf-trip <egornesvickij@gmail.com>
2023-06-15 14:36:21 +0500

    Connected api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
+import { ServerData } from './weatherResponseType';
+import { getWeatherRequest } from './api/weatherRequest.api';
 
 function App() {
+  const [weatherData, setWeatherData] = useState<ServerData | undefined>();
+
+  useEffect(() => {
+    if (weatherData === undefined) getWeatherRequest(setWeatherData);
+  }, []);
+
   return (
     <>
       <div>APP</div>
+      <h1>{weatherData?.location.name}</h1>
+      <h2>{weatherData?.current.temp_c}C</h2>
     </>
   );
 }
---
+++ src/api/weatherRequest.api.ts
@@ -0,0 +1,17 @@
+import { Dispatch, SetStateAction } from "react";
+import { ServerData, ServerResponse } from "../weatherResponseType";
+import axios from "axios";
+
+const url = 'https://api.weatherapi.com/v1/current.json?key=41dab428dc624be0b63184944231406&q=Екатеринбург&aqi=no';
+
+
+export const getWeatherRequest = (
+  setWeatherData: Dispatch<SetStateAction<ServerData | undefined>>
+) => {
+  axios
+    .get(url)
+    .then((response: ServerResponse) => {
+      setWeatherData(response.data);
+    })
+    .catch((error: any) => console.log(error));
+};
