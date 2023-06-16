24f72dee47adc4497bfe74b937f4f5450121ce1f
snaf-trip <egornesvickij@gmail.com>
2023-06-16 14:54:57 +0500

    added loader animation

--- src/components/weatherContnent/weatherContent.components.tsx
+++ src/components/weatherContnent/weatherContent.components.tsx
@@ -1,5 +1,6 @@
 import './weatherContent.components.scss';
 import { ServerData } from '../../weatherResponseType';
+import { Loader } from '../loader/loader.components';
 
 interface Props {
   data: ServerData | undefined;
@@ -8,15 +9,21 @@ interface Props {
 export const WeatherContent = ({ data }: Props) => {
   return (
     <div className="weatherContent__container">
-      <img
-        src={`src/weatherIcons/${data?.current.condition.text}.svg`}
-        alt=""
-        width={250}
-        height={250}
-        className="weatherContent__weatherIcon"
-      />
-      <div className="weatherContent__temp">{data?.current.temp_c}°</div>
-      <div className="weatherContent__otherInfo">Other content</div>
+      {data === undefined ? (
+        <Loader />
+      ) : (
+        <>
+          <img
+            src={`src/weatherIcons/${data?.current.condition.text}.svg`}
+            alt=""
+            width={250}
+            height={250}
+            className="weatherContent__weatherIcon"
+          />
+          <div className="weatherContent__temp">{data?.current.temp_c}°</div>
+          <div className="weatherContent__otherInfo">Other content</div>
+        </>
+      )}
     </div>
   );
 };
