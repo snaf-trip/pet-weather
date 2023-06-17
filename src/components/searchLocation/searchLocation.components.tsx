74b09049e160a3e79265d6c6fb92ede160da4506
snaf-trip <egornesvickij@gmail.com>
2023-06-17 13:54:20 +0500

    get weather by input

--- src/components/searchLocation/searchLocation.components.tsx
+++ src/components/searchLocation/searchLocation.components.tsx
@@ -1,18 +1,30 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './searchLocation.components.scss';
 import { LocationIcon } from './icons/locationIcon';
 import { SearchIcon } from './icons/searchIcon';
+import { getWeatherRequest } from '../../api/weatherRequest.api';
+import weatherDataStoreStore from '../../stores/weatherDataStore.stores';
 
 export const SearchLocation: FC = () => {
+  const [location, setLocation] = useState<string | undefined>();
+  const { getData } = weatherDataStoreStore;
+
+  const updateLocation = () => {
+    getWeatherRequest(location!, getData);
+  };
+
   return (
     <div className="searchLocation__container">
       <LocationIcon />
       <input
         type="text"
         className="searchLocation__input"
-        value={'Чебоксары'}
+        onChange={(e) => setLocation(e.target.value)}
       />
-      <SearchIcon className="searchLocation__searchIcon" />
+      <SearchIcon
+        className="searchLocation__searchIcon"
+        onClick={() => updateLocation()}
+      />
     </div>
   );
 };
