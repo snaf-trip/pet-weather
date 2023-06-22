a2c4379039b9b533919d38b7296e6c1023301b73
snaf-trip <egornesvickij@gmail.com>
2023-06-22 20:57:13 +0500

    added deploy.sh for gh-pages

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# abort on errors
+set -e
+
+# load .env
+eval "$(cat .env <(echo) <(declare -x))"
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+# if you are deploying under a user then do:
+# git push -f https://github.com/<USERNAME>/<REPO>.git master:gh-pages
+
+# if you are deploying under an organization then do:
+# git push -f https://github.com/snaf-trip/pet-weather.git master:gh-pages
+
+cd -
